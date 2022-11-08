2345d8782bdc224bfd360453b90502aa8893c93a
Johnhenryla <jhwatler@gmail.com>
2022-11-08 12:48:28 -0800

    First couple days of web fun

---
+++ w1d2.js
@@ -0,0 +1,25 @@
+// var a = 25;
+// a = a - 13;
+// // console.log(a/2);
+// // console.log(a);
+
+// a = "hello";
+
+
+// for(var i = 0; i <10; i++){
+//     console.log(i)
+//     i = i + 3;
+// }
+// console.log('outside the loop'+ i);
+
+
+function gettotal(arrayofnumbers){
+    var sum = arrayofnumbers[0];
+
+    for(var i = 0; i < arrayofnumbers.length; i++){
+        sum += arrayofnumbers[i];
+        console.log(`the current sum is:""{sum}`)
+
+    }
+    console.log("the total is: " + sum)
+    }
