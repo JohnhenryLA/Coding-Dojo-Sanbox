9762778f98915cdb3628c5b97824f65babbe174a
Johnhenryla <jhwatler@gmail.com>
2022-11-14 09:09:07 -0800

    finished code

---
+++ Morning Algo/w2d1.js
@@ -0,0 +1,42 @@
+function d6(){
+    var roll = Math.floor(Math.random() * 6) +1;
+
+    //should return 1,2,3,4,5, or 6
+    return roll;
+}
+
+var playerRoll = d6();
+console.log(`The player rolled a ${playerRoll}!!`);
+
+//write a bit of code to randomly print an option from the following array
+var lifesAnswers = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes - definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+];
+
+function magic8(Fun){
+    var index = Math.floor(Math.random() * Fun.length);
+    console.log (index);
+
+    return Fun[index];
+}
+
+console.log(magic8(lifesAnswers));
